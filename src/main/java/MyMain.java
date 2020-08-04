45794e0a2ce0749952c9da5a63c240c92a169b70
Nathan Lin <nathanlukelin@gmail.com>
2020-08-04 18:18:38 +0000

    Edited Main

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,15 +1,6 @@
-// import java.util.Scanner;
-
 public class MyMain {
     public static void main(String[] args) {
         // Write some code here!
-        System.out.println("Hello, World!");
 
-        // Scanner scan = new Scanner(System.in);
-        // System.out.print("What is your name? ");
-        // String name = scan.next();
-
-        // System.out.println("Hello, " + name + "!");
-        // scan.close();
     }
 }
